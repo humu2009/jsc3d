359c20ba26e93b8c7b3c38840c14ecb5bbe75b6e
humu2009 <humu2009@2bce3d21-d416-05c5-016d-b38e958221ee>
2011-08-31 08:15:50 +0000

    building scripts have been added

--- trunk/jsc3d/build/compile.bat
+++ trunk/jsc3d/build/compile.bat
@@ -1,2 +1,1 @@
-java -jar  compiler.jar --js "..\jsc3d.js" --js_output_file "..\bin\jsc3d.min.js"
-pause
+java -jar  ../tools/closure/compiler.jar --js "../jsc3d.js" --js_output_file "../jsc3d.min.js"
---
+++ trunk/jsc3d/build/compile.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar  ../tools/closure/compiler.jar --js ../jsc3d.js --js_output_file ../jsc3d.min.js
